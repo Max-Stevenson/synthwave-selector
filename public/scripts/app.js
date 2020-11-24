e89d9484b92261349d908a39bde6737a53cbf0e5
Max Stevenson <maxstevenson@msn.com>
2020-11-23 21:54:24 -0600

    Refactor of loading bar
    - Tidied up loading bar function.
    - Moved overall loading elements into container div.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,37 +11,20 @@ anime({
 });
 
 const progressLoadingBar = () => {
-  let i = 0;
-  if (i == 0) {
-    i = 1;
-    let elem = document.getElementById("loading-bar__bar");
-    let width = 1;
-    let id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
+  let progressBar = document.getElementById("loading-bar__bar");
+  let progress = parseInt(progressBar.getAttribute("progress"));
+  let id = setInterval(frame, 50);
+
+  function frame() {
+    if (progress >= 100) {
+      clearInterval(id);
+      progressBar.style.display = "none";
+    } else {
+      progress++;
+      progressBar.style.width = progress + "%";
+      progressBar.setAttribute("progress", progress);
     }
-  }
+  };
 };
 
-const loadingAnimation = () => {
-  progressLoadingBar();
-}
-
-
-window.onload = function (e) {
-  var width = 0;
-
-  setInterval(function () {
-
-    width = width >= 100 ? 0 : width + 5;
-    document.getElementById('loading-bar__bar').style.width = width + '%';
-
-
-  }, 10)
-};
+progressLoadingBar();

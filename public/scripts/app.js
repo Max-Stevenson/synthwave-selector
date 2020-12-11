2a6b877c129d06155eb22d1ae11c11b20c5729a7
Max Stevenson <maxstevenson@msn.com>
2020-12-11 17:07:31 -0600

    Ripping decision tree code off the net.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -319,6 +319,51 @@ $(function () {
 
 });
 
+class Tree {
+  constructor(treeObj) {
+    this.nodes = [];
+    this.currentNode = 0;
+
+    const add = ({ message, yes, no }) => {
+      const currentIndex = this.nodes.length;
+      this.nodes.push({ message });
+      if (yes) {
+        this.nodes[currentIndex].yesIndex = add(yes);
+      }
+      if (no) {
+        this.nodes[currentIndex].noIndex = add(no);
+      }
+      return currentIndex;
+    };
+
+    add(treeObj);
+  }
+
+  setCurrentNode(node) {
+    this.currentNode = node;
+  }
+
+  getCurrentNode() {
+    return this.currentNode;
+  }
+
+  getCurrentNodeMessage() {
+    return this.nodes[this.currentNode].message;
+  }
+  
+  walk(affirmative) {
+    if (affirmative) {
+      this.currentNode = this.nodes[this.currentNode].yesIndex;
+    } else {
+      this.currentNode = this.nodes[this.currentNode].noIndex;
+    }
+    return {
+      message: this.getCurrentNodeMessage(),
+      complete: !this.nodes[this.currentNode].yesIndex,
+    };
+  }
+}
+
 const treeData = {
   message: "Do you want vocals with your synthwave?",
   yes: {
@@ -339,13 +384,14 @@ const treeData = {
     yes: {
       answer: "I've been known to brood...",
       message: "Dark or Cyber?",
-      yes: { result: "Darksynth" },
-      no: { result: "Cyberpunk" },
+      yes: { answer: "Let's go dark", result: "Darksynth" },
+      no: { answer: "Give me something to go with the bleak future", result: "Cyberpunk" },
     },
     no: {
+      answer: "I'm a ray of sunshine",
       message: "To the stars?",
-      yes: { result: "Spacewave" },
-      no: { message: "Driving fast?", yes: { result: "Outrun" }, no: { result: "Dreamwave" } }
+      yes: { answer: "Get me off this rock", result: "Spacewave" },
+      no: { answer: "I'd prefer to stay local", message: "Driving fast?", yes: { answer: "I'm fast af boi", result: "Outrun" }, no: { answer: "I prefer to take my time", result: "Dreamwave" } }
     },
   },
 };
